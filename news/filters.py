03524258331c5bd13543aeb1e76db8c4e493e4b9
RuslanF <voxvt@mail.ru>
2023-12-26 01:11:39 +0500

    first commit

---
+++ news/filters.py
@@ -0,0 +1,12 @@
+from django_filters import FilterSet, DateFilter
+from .models import Post, Author
+
+class PostFilter(FilterSet):
+
+    class Meta:
+        model = Post
+        fields = {
+            'post_title': ['icontains'],
+            'author': ['exact'],
+            'created_at': ['gt']
+        }
